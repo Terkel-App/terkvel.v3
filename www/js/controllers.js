486591d1b5dce2dcd0406ad4b5fc147682c100b5
YOUR NAME <mypeopl3@gmail.com>
2016-04-20 11:36:08 +0100

    commit 1

---
+++ www/js/controllers.js
@@ -0,0 +1,22 @@
+angular.module('app.controllers', [])
+  
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('dashbordCtrl', function($scope) {
+
+})
+   
+.controller('ukeplanCtrl', function($scope) {
+
+})
+   
+.controller('lekseplanCtrl', function($scope) {
+
+})
+      
+.controller('fravRCtrl', function($scope) {
+
+})
+ 
